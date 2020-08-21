6238e33f9b0a5c33bac50f5894ff42879b90a9cf
A <hallo@alexanderboenninger.de>
2020-08-21 11:20:18 +0200

    update

--- app.js
+++ app.js
@@ -5,7 +5,51 @@ const strText = text.textContent;
 const splitText = strText.split("");
 text.textContent = "";
 
+function initialize() {
+  //we set the scroll handler here
+  window.addEventListener('scroll', onScroll)
+}
+
+function onScroll() {
+  //run this function on every scroll event
+  if(elementScrolled('.banner')) {
+    //if the elment is in the viewport, remove the scroll listener, and run runAnimation()
+    //this way we guarantee the runAnimation function is executed only once
+    window.removeEventListener('scroll', onScroll);
+    runAnimation();
+  }
+}
+
+function runAnimation() {
+  for (let i = 0; i < splitText.length; i++){
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+  }
+
+  let char = 0;
+  let timer = setInterval(onTick, 100);
+
+  function onTick(){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('blick');
+    back.classList.toggle('black');
+    char++
+
+    if (char === splitText.length){
+      complete();
+      return;
+    }
+  }
+
+  function complete() {
+    clearInterval(timer);
+    timer = null;
+    back.classList.remove('black');
+
+  }
+}
+
 function elementScrolled(elem) {
+  //helper function to see if element is in viewport
   var docViewTop = $(window).scrollTop();
   var docViewBottom = docViewTop + $(window).height()/1.5;
   var elemTop = $(elem).offset().top;
@@ -13,37 +57,4 @@ function elementScrolled(elem) {
   return ((elemTop <= docViewBottom) && (thisH >= docViewTop));
 }
 
-function onScroll() {
-  if(elementScrolled('.banner')) {
-    for (let i = 0; i < splitText.length; i++){
-      text.innerHTML += "<span>" + splitText[i] + "</span>";
-    }
-
-    let char = 0;
-    let timer = setInterval(onTick, 100);
-
-    function onTick(){
-      const span = text.querySelectorAll('span')[char];
-      span.classList.add('blick');
-      back.classList.toggle('black');
-      char++
-
-      if (char === splitText.length){
-        complete();
-        return;
-      }
-    }
-
-    function complete(){
-      clearInterval(timer);
-      timer = null;
-      back.classList.remove('black');
-      back.classList.remove('banner');
-      window.removeEventListener('scroll', onScroll);
-    }
- }
-}
-
-
-
-window.addEventListener('scroll', onScroll)
+initialize();
